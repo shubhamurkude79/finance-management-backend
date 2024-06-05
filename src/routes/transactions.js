aed5289fc69075599ad37715ccaac5e12c201d49
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-06-06 00:09:50 +0530

    update of try and catch block in transactions routes

--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -3,14 +3,22 @@ const router = express.Router();
 const Transaction = require('../models/Transaction');
 
 router.get('/', async (req, res) => {
-    const transactions = await Transaction.find();
-    res.json(transactions);
+    try {
+        const transactions = await Transaction.find();
+        res.status(200).send(transactions);
+      } catch (error) {
+        res.status(500).send(error);
+      }
 });
 
 router.post('/', async (req, res) => {
     const transaction = new Transaction(req.body);
-    await transaction.save();
-    res.status(201).json(transaction);
+    try {
+        await transaction.save();
+        res.status(201).send(transaction);
+      } catch (error) {
+        res.status(400).send(error);
+      }    
 });
 
 module.exports = router;
