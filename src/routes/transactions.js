c1ecb82fbff6fb8c2f9b13106899d94359ad6847
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-06-13 00:41:12 +0530

    commented code removed

--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -7,9 +7,7 @@ router.get('/', async (req, res) => {
     try {
         const transactions = await db.collection('transactions').find({}).toArray();
         res.json(transactions);
-        // res.status(200).send(transactions);
       } catch (err) {
-        // res.status(500).send(error);
         console.error('Failed to get transactions:', err);
         res.status(500).json({ error: 'Failed to get transactions' });
       }
@@ -18,14 +16,10 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
   const db = req.app.locals.db;
   const newTransaction = new Transaction(req.body);
-    // const transaction = new Transaction(req.body);
     try {
-        // await transaction.save();
-        // res.status(201).send(transaction);
         const result = await db.collection('transactions').insertOne(newTransaction);
         res.status(201).json(result.ops[0]);
       } catch (err) {
-        // res.status(400).send(error);
         console.error('Failed to add transaction:', err);
         res.status(500).json({ error: 'Failed to add transaction' });
       }    
